f4b7decf0c2c7edf96a3059fe19375ab9d27f6fd
tldk42 <tldk47@hanmail.net>
2023-09-05 13:18:49 +0900

    + TCMovementComponent (Base)

---
+++ Source/TheCrusader/Private/Character/Movement/TCMovementComponent.cpp
@@ -0,0 +1,130 @@
+﻿// Written by jaegang lim
+
+
+#include "Character/Movement/TCMovementComponent.h"
+
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/Character.h"
+
+namespace TheCrusader
+{
+	static float GroundTraceDistance = 100000.0f;
+	FAutoConsoleVariableRef CVar_GroundTraceDistance(
+		TEXT("TheCrusader.GroundTraceDistance"), GroundTraceDistance,
+		TEXT("Distance to trace down when generating ground information."), ECVF_Cheat);
+}
+
+UTCMovementComponent::UTCMovementComponent()
+{
+}
+
+const FTCGroundInfo& UTCMovementComponent::GetGroundInfo()
+{
+	// 전 프레임과 동일한 상태면 SKIP
+	if (!CharacterOwner || (GFrameCounter == CachedGroundInfo.LastUpdateFrame))
+		return CachedGroundInfo;
+
+	// 땅위에서 걷고 있으면 HitResult 초기화
+	if (MovementMode == MOVE_Walking)
+	{
+		CachedGroundInfo.GroundHitResult = CurrentFloor.HitResult;
+		CachedGroundInfo.GroundDistance = 0.f;
+	}
+	else
+	{
+		const UCapsuleComponent* Capsule = CharacterOwner->GetCapsuleComponent();
+		check(Capsule);
+
+		// Line Trace (캐릭터 캡슐 중간 부터 -100000 까지) 
+		const float CapsuleHalfHeight = Capsule->GetUnscaledCapsuleHalfHeight();
+		const ECollisionChannel CollisionChannel = (UpdatedComponent
+			                                            ? UpdatedComponent->GetCollisionObjectType()
+			                                            : ECC_Pawn);
+		const FVector TraceStart(GetActorLocation());
+		const FVector TraceEnd(TraceStart.X, TraceStart.Y,
+		                       (TraceStart.Z - TheCrusader::GroundTraceDistance - CapsuleHalfHeight));
+
+		FCollisionQueryParams QueryParams;
+		QueryParams.AddIgnoredActor(CharacterOwner);
+		QueryParams.bTraceComplex = false;
+		FCollisionResponseParams ResponseParams;
+		InitCollisionParams(QueryParams, ResponseParams);
+
+		FHitResult HitResult;
+		GetWorld()->LineTraceSingleByChannel(HitResult, TraceStart, TraceEnd, CollisionChannel, QueryParams,
+		                                     ResponseParams);
+
+		// 정보 업데이트
+		CachedGroundInfo.GroundHitResult = HitResult;
+		CachedGroundInfo.GroundDistance = TheCrusader::GroundTraceDistance;
+
+		if (MovementMode == MOVE_NavWalking)
+		{
+			CachedGroundInfo.GroundDistance = .0f;
+		}
+		else if (HitResult.bBlockingHit)
+		{
+			CachedGroundInfo.GroundDistance = FMath::Max((HitResult.Distance - CapsuleHalfHeight), .0f);
+		}
+	}
+
+	CachedGroundInfo.LastUpdateFrame = GFrameCounter;
+
+	return CachedGroundInfo;
+}
+
+float UTCMovementComponent::SlideAlongSurface(const FVector& Delta, float Time, const FVector& InNormal,
+                                              FHitResult& Hit, bool bHandleImpact)
+{
+	// Normal opposing movement, along which we will slide. (즉, 슬라이드 될 방향 / 표면의 법선)
+	FVector Normal(InNormal);
+
+	// 이 MovementComp와 부딪힌 개체가 Chacracter클래스인가?
+	const bool bHitCharacter = Hit.GetHitObjectHandle().DoesRepresentClass(ACharacter::StaticClass());
+	bLastSurfaceWasCharacter = bHitCharacter;
+	const float EffectiveMinHorizontalSurfaceSlideAngle = bHitCharacter
+		                                                      ? MinHorizontalSurfaceSlideAngleCharacter
+		                                                      : MinHorizontalSurfaceSlideAngle;
+
+	if (EffectiveMinHorizontalSurfaceSlideAngle > 0.f)
+	{
+		const FVector MovementDirection = Acceleration.GetSafeNormal2D();
+
+		// 입력방향 | 슬라이딩 방향 내적 -> 두 벡터 사이의 각도
+		const float MovementDotNormal = MovementDirection | Normal;
+		if (FMath::Abs(MovementDotNormal) >
+			FMath::Cos(FMath::DegreesToRadians(EffectiveMinHorizontalSurfaceSlideAngle)))
+		{
+			Normal = -MovementDirection;
+			TimeLastSlideAlongSurfaceBlock = GetWorld()->GetTimeSeconds();
+		}
+
+#if !(UE_BUILD_SHIPPING || UE_BUILD_TEST)
+
+		const float DebugAngle = FMath::RadiansToDegrees(FMath::Acos(FMath::Abs(MovementDotNormal)));
+		DrawDebugLine(GetWorld(), CharacterOwner->GetActorLocation(),
+		              CharacterOwner->GetActorLocation() + Normal * 30.0f, FColor::Red, false, -1.0f,
+		              SDPG_Foreground, 2.0f);
+		DrawDebugLine(GetWorld(), CharacterOwner->GetActorLocation(),
+		              CharacterOwner->GetActorLocation() + MovementDirection * 30.0f, FColor::Blue, false, -1.0f,
+		              SDPG_Foreground, 2.0f);
+		DrawDebugString(GetWorld(), CharacterOwner->GetActorLocation(), FString::SanitizeFloat(DebugAngle), nullptr,
+		                FColor::White, 0.0f, false, 2.0f);
+
+#endif
+	}
+
+
+	return Super::SlideAlongSurface(Delta, Time, Normal, Hit, bHandleImpact);
+}
+
+bool UTCMovementComponent::WasSlideAlongSurfaceBlockedRecently(float Tolerance) const
+{
+	if (const UWorld* const World = GetWorld())
+	{
+		const float TimeThreshold = FMath::Max(Tolerance, World->DeltaTimeSeconds + KINDA_SMALL_NUMBER);
+		return World->TimeSince(TimeLastSlideAlongSurfaceBlock) <= TimeThreshold;
+	}
+
+	return false;
+}
---
+++ Source/TheCrusader/Public/Character/Movement/TCMovementComponent.h
@@ -0,0 +1,73 @@
+﻿// Written by jaegang lim
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "TCMovementComponent.generated.h"
+
+class ATheCrusaderCharacter;
+
+USTRUCT(BlueprintType)
+struct FTCGroundInfo
+{
+	GENERATED_BODY()
+
+	FTCGroundInfo()
+		: LastUpdateFrame(0),
+		  GroundDistance(0)
+	{
+		
+	}
+
+	uint64 LastUpdateFrame;
+
+	UPROPERTY(BlueprintReadOnly)
+	FHitResult GroundHitResult;
+
+	UPROPERTY(BlueprintReadOnly)
+	float GroundDistance;
+};
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class THECRUSADER_API UTCMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+
+	float TimeLastSlideAlongSurfaceBlock = -1000.0f;
+
+public:
+	UTCMovementComponent();
+
+	/** Ground 정보를 반환 */
+	UFUNCTION(BlueprintCallable, Category = "EssentialData")
+	const FTCGroundInfo& GetGroundInfo();
+
+protected:
+	// SlideAlongSurface 는 이동하다가 충돌 발생시 그냥 제자리에 멈춰 벽이나 경사로에 "달라붙기" 보다는, 그 표면을 타고 부드럽게 미끄러지도록 하는 데 관련된 물리 계산 처리를 합니다.
+	virtual float SlideAlongSurface(const FVector& Delta, float Time, const FVector& InNormal, FHitResult& Hit,
+									bool bHandleImpact) override;
+public:
+	UFUNCTION(BlueprintCallable)
+	bool WasSlideAlongSurfaceBlockedRecently(float Tolerance) const;
+
+protected:
+	// ConstraintInputAcceleration will interpolate the input size to go from this value to 1.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MinInputAccelerationSize = 0.0f;
+
+	// Minimum angle for movement to slide against. This prevents sliding against very sharp angles that will result in very small movements. Only active if greater than 0.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MinHorizontalSurfaceSlideAngle = 35.f;
+
+	// Minimum angle for movement to slide against when colliding against a character
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MinHorizontalSurfaceSlideAngleCharacter = 20.f;
+
+	UPROPERTY(Transient, BlueprintReadOnly)
+	bool bLastSurfaceWasCharacter = false;
+
+	// Cached ground info for the character.  Do not access this directly!  It's only updated when accessed via GetGroundInfo().
+	FTCGroundInfo CachedGroundInfo;
+};
