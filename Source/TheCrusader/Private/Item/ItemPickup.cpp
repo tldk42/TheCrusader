929bd954d2af2ee0001e5b7df58bfde13803a2bc
tldk42 <tldk47@hanmail.net>
2023-09-14 10:41:15 +0900

    + PickupItem(Test)

---
+++ Source/TheCrusader/Private/Item/ItemPickup.cpp
@@ -0,0 +1,159 @@
+﻿// Written by jaegang lim
+
+
+#include "Item/ItemPickup.h"
+
+#include "Character/Balian.h"
+#include "Component/Inventory/InventoryComponent.h"
+#include "Item/ItemBase.h"
+
+
+AItemPickup::AItemPickup()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	PickupMesh = CreateDefaultSubobject<UStaticMeshComponent>("PickupMesh");
+	PickupMesh->SetSimulatePhysics(true);
+	SetRootComponent(PickupMesh);
+}
+
+void AItemPickup::BeginPlay()
+{
+	Super::BeginPlay();
+
+	InitializePickup(UItemBase::StaticClass(), ItemQuantity);
+}
+
+void AItemPickup::BeginFocus()
+{
+	if (PickupMesh)
+	{
+		PickupMesh->SetRenderCustomDepth(true);
+	}
+}
+
+void AItemPickup::EndFocus()
+{
+	if (PickupMesh)
+	{
+		PickupMesh->SetRenderCustomDepth(false);
+	}
+}
+
+void AItemPickup::BeginInteract()
+{
+}
+
+void AItemPickup::EndInteract()
+{
+}
+
+void AItemPickup::Interact(ABalian* PlayerCharacter)
+{
+	if (PlayerCharacter)
+	{
+		TakePickup(PlayerCharacter);
+	}
+}
+
+void AItemPickup::InitializePickup(const TSubclassOf<UItemBase> BaseClass, const int32 InQuantity)
+{
+	if (ItemDataTable && !DesiredItemID.IsNone())
+	{
+		const FItemData* ItemData = ItemDataTable->FindRow<FItemData>(DesiredItemID, DesiredItemID.ToString());
+
+		ItemRef = NewObject<UItemBase>(this, BaseClass);
+
+		ItemRef->ID = ItemData->ItemID;
+		ItemRef->ItemType = ItemData->ItemType;
+		ItemRef->ItemQuality = ItemData->ItemQuality;
+		ItemRef->NumericData = ItemData->NumericData;
+		ItemRef->TextData = ItemData->TextData;
+		ItemRef->AssetData = ItemData->AssetData;
+
+		InQuantity <= 0 ? ItemRef->SetQuantity(1) : ItemRef->SetQuantity(InQuantity);
+
+		PickupMesh->SetStaticMesh(ItemData->AssetData.Mesh);
+
+		UpdateInteractableData();
+	}
+}
+
+void AItemPickup::InitializeDrop(UItemBase* ItemToDrop, int32 InQuantity)
+{
+	ItemRef = ItemToDrop;
+	InQuantity <= 0 ? ItemRef->SetQuantity(1) : ItemRef->SetQuantity(InQuantity);
+	ItemRef->NumericData.Weight = ItemToDrop->GetItemSingleWeight();
+	PickupMesh->SetStaticMesh(ItemToDrop->AssetData.Mesh);
+	UpdateInteractableData();
+}
+
+void AItemPickup::TakePickup(const ABalian* Taker)
+{
+	if (!IsPendingKillPending()) // ???
+	{
+		if (ItemRef)
+		{
+			if (UInventoryComponent* PlayerInventory = Taker->GetInventory())
+			{
+				// 플레이어 인벤토리에 아이템 추가해야함
+				const FItemAddResult AddResult = PlayerInventory->HandleAddItem(ItemRef);
+
+				switch (AddResult.OperationResult)
+				{
+				case EItemAddResult::NoItemAdded:
+					break;
+				case EItemAddResult::PartialAmountItemAdded:
+					UpdateInteractableData();
+					Taker->UpdateInteractionWidget();
+					break;
+				case EItemAddResult::AllTemAdded:
+					Destroy();
+					break;
+				default: ;
+				}
+
+				UE_LOG(LogTemp, Warning, TEXT("%s"), *AddResult.ResultMessage.ToString());
+			}
+			else
+			{
+				UE_LOG(LogTemp, Warning, TEXT("Player inventory component is null"));
+			}
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Pickup internal item reference was somehow null!"));
+		}
+	}
+}
+
+void AItemPickup::UpdateInteractableData()
+{
+	InstanceInteractableData.InteractableType = EInteractableType::Pickup;
+	InstanceInteractableData.Action = ItemRef->TextData.InteractionText;
+	InstanceInteractableData.Name = ItemRef->TextData.Name;
+	InstanceInteractableData.Quantity = ItemRef->Quantity;
+	InteractableData = InstanceInteractableData;
+}
+
+void AItemPickup::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	const FName ChangedPropertyName = PropertyChangedEvent.Property
+		                                  ? PropertyChangedEvent.Property->GetFName()
+		                                  : NAME_None;
+
+	if (ChangedPropertyName == GET_MEMBER_NAME_CHECKED(AItemPickup, DesiredItemID))
+	{
+		if (ItemDataTable)
+		{
+			const FString ContextString{DesiredItemID.ToString()};
+
+			if (const FItemData* ItemData = ItemDataTable->FindRow<FItemData>(DesiredItemID, DesiredItemID.ToString()))
+			{
+				PickupMesh->SetStaticMesh(ItemData->AssetData.Mesh);
+			}
+		}
+	}
+}
---
+++ Source/TheCrusader/Public/Item/ItemPickup.h
@@ -0,0 +1,64 @@
+﻿// Written by jaegang lim
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interfaces/Interactable.h"
+#include "ItemPickup.generated.h"
+
+class UItemBase;
+class UDataTable;
+
+UCLASS()
+class THECRUSADER_API AItemPickup : public AActor, public IInteractable
+{
+	GENERATED_BODY()
+
+public:
+	AItemPickup();
+
+public:
+#pragma region IInteractabale
+	virtual void BeginFocus() override;
+	virtual void EndFocus() override;
+	virtual void BeginInteract() override;
+	virtual void EndInteract() override;
+	virtual void Interact(ABalian* PlayerCharacter) override;
+#pragma endregion IInteractabale
+
+	void InitializePickup(const TSubclassOf<UItemBase> BaseClass, const int32 InQuantity);
+	void InitializeDrop(UItemBase* ItemToDrop, int32 InQuantity);
+
+	FORCEINLINE UItemBase* GetItemData() const { return ItemRef; }
+
+protected:
+	virtual void BeginPlay() override;
+
+protected:
+	void TakePickup(const ABalian* Taker);
+	void UpdateInteractableData();
+
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+	
+protected:
+	UPROPERTY(VisibleAnywhere, Category = "Pickup | Components")
+	UStaticMeshComponent* PickupMesh;
+
+	UPROPERTY(EditInstanceOnly, Category = "Pickup | Item Initialization")
+	UDataTable* ItemDataTable;
+
+	UPROPERTY(EditInstanceOnly, Category = "Pickup | Item Initialization")
+	FName DesiredItemID;
+
+	UPROPERTY(VisibleAnywhere, Category = "Pickup | Item Reference")
+	UItemBase* ItemRef;
+
+	UPROPERTY(EditInstanceOnly, Category = "Pickup | Item Initialization")
+	int32 ItemQuantity;
+
+	UPROPERTY(VisibleAnywhere, Category = "Pickup | Interaction")
+	FInteractableData InstanceInteractableData;
+};
