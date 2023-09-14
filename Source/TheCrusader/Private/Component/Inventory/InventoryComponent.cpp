a86d56caca1dd24c9e0e907505faa43aee08934e
tldk42 <tldk47@hanmail.net>
2023-09-14 10:40:52 +0900

    + InventoryComponent

---
+++ Source/TheCrusader/Private/Component/Inventory/InventoryComponent.cpp
@@ -0,0 +1,214 @@
+﻿// Written by jaegang lim
+
+#include "Component/Inventory/InventoryComponent.h"
+
+#include "Item/ItemBase.h"
+
+
+UInventoryComponent::UInventoryComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UInventoryComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+UItemBase* UInventoryComponent::FindMatchingItem(UItemBase* ItemIn) const
+{
+	if (ItemIn)
+	{
+		if (InventoryContents.Contains(ItemIn))
+		{
+			return ItemIn;
+		}
+	}
+	return nullptr;
+}
+
+UItemBase* UInventoryComponent::FindNextItemByID(UItemBase* ItemIn) const
+{
+	if (ItemIn)
+	{
+		if (const TArray<TObjectPtr<UItemBase>>::ElementType* Result = InventoryContents.FindByKey(ItemIn))
+		{
+			return *Result;
+		}
+	}
+	return nullptr;
+}
+
+UItemBase* UInventoryComponent::FindNextPartialStack(UItemBase* ItemIn) const
+{
+	// FindByPredict는 Pred에 일치하는 첫 요소를 반환함.
+	// InventoryContents에서 Iterating하면서 탐색 
+	if (const TArray<TObjectPtr<UItemBase>>::ElementType* Result =
+		InventoryContents.FindByPredicate([&ItemIn](const UItemBase* InventoryItem) // InventoryItem -> *It
+		{
+			return InventoryItem->ID == ItemIn->ID && !InventoryItem->IsFullItemStack();
+		}))
+	{
+		return *Result;
+	}
+	return nullptr;
+}
+
+int32 UInventoryComponent::CalculateWeightAddAmount(UItemBase* ItemIn, int32 RequestedAddAmount)
+{
+	const int32 WeightMaxAddAmount = FMath::FloorToInt(
+		(GetWeightCapacity() - InventoryTotalWeight) / ItemIn->GetItemSingleWeight());
+
+	if (WeightMaxAddAmount >= RequestedAddAmount)
+	{
+		return RequestedAddAmount;
+	}
+	return WeightMaxAddAmount;
+}
+
+int32 UInventoryComponent::CalculateNumberForFullStack(UItemBase* StackableItem, int32 RequestedAddAmount)
+{
+	const int32 AddAmountToMakeFullStack = StackableItem->NumericData.MaxStackSize - StackableItem->Quantity;
+
+	return FMath::Min(RequestedAddAmount, AddAmountToMakeFullStack);
+}
+
+
+void UInventoryComponent::RemoveSingleInstanceOfItem(UItemBase* ItemToRemove)
+{
+	InventoryContents.RemoveSingle(ItemToRemove);
+	OnInventoryUpdated.Broadcast();
+}
+
+int32 UInventoryComponent::RemoveAmountOfItem(UItemBase* ItemToRemove, int32 DesiredAmountToRemove)
+{
+	const int32 ActualAmountToRemove = FMath::Min(DesiredAmountToRemove, ItemToRemove->Quantity);
+
+	ItemToRemove->SetQuantity(ItemToRemove->Quantity - ActualAmountToRemove);
+
+	InventoryTotalWeight -= ActualAmountToRemove * ItemToRemove->GetItemSingleWeight();
+
+	OnInventoryUpdated.Broadcast();
+
+	return ActualAmountToRemove;
+}
+
+void UInventoryComponent::SplitExistingStack(UItemBase* ItemIn, const int32 AmountToSplit)
+{
+	if (InventoryContents.Num() + 1 <= InventorySlotCapacity)
+	{
+		RemoveAmountOfItem(ItemIn, AmountToSplit);
+		AddNewItem(ItemIn, AmountToSplit);
+	}
+}
+
+int32 UInventoryComponent::HandleStackableItems(UItemBase* ItemIn, int32 RequestedAddAmount)
+{
+	return 0;
+}
+
+FItemAddResult UInventoryComponent::HandleNonStackableItems(UItemBase* ItemIn, int32 RequestedAddAmount)
+{
+	if (FMath::IsNearlyZero(ItemIn->GetItemSingleWeight()) || ItemIn->GetItemSingleWeight() < 0)
+	{
+		// return added none
+		return FItemAddResult::AddedNone(
+			FText::Format(FText::FromString("Could not add {0} to the inventory. Item has invalid weight value."),
+			              ItemIn->TextData.Name));
+	}
+
+	// overflow weight
+	if (InventoryTotalWeight + ItemIn->GetItemSingleWeight() > GetWeightCapacity())
+	{
+		// return added none
+		return FItemAddResult::AddedNone(
+			FText::Format(FText::FromString("Could not add {0} to the inventory. Item has overflow weight."),
+			              ItemIn->TextData.Name));
+	}
+
+	// overflow slot
+	if (InventoryContents.Num() + 1 > InventorySlotCapacity)
+	{
+		// return added none
+		return FItemAddResult::AddedNone(
+			FText::Format(FText::FromString("Could not add {0} to the inventory. Item has overflow slot."),
+			              ItemIn->TextData.Name));
+	}
+
+	AddNewItem(ItemIn, RequestedAddAmount);
+
+	// return added all result
+	return FItemAddResult::AddedAll(
+		FText::Format(FText::FromString("Successfully added {0} {1} to the inventory."),
+		              RequestedAddAmount, ItemIn->TextData.Name), RequestedAddAmount);
+}
+
+
+FItemAddResult UInventoryComponent::HandleAddItem(UItemBase* InputItem)
+{
+	if (GetOwner())
+	{
+		const int32 InitialRequestedAddAmount = InputItem->Quantity;
+
+		if (!InputItem->NumericData.bIsStackable)
+		{
+			return HandleNonStackableItems(InputItem, InitialRequestedAddAmount);
+		}
+
+		const int32 StackableAmountAdded = HandleStackableItems(InputItem, InitialRequestedAddAmount);
+
+		if (StackableAmountAdded == InitialRequestedAddAmount)
+		{
+			// return all result
+			return FItemAddResult::AddedAll(FText::Format(
+				                                FText::FromString("Successfully added {0} {1} to the inventory."),
+				                                InitialRequestedAddAmount,
+				                                InputItem->TextData.Name),
+			                                InitialRequestedAddAmount);
+		}
+		if (StackableAmountAdded < InitialRequestedAddAmount && StackableAmountAdded > 0)
+		{
+			// return partial result
+			return FItemAddResult::AddedPartial(FText::Format(
+				                                    FText::FromString(
+					                                    "Partial amount of {0} added to the inventory. Number added = {1}"),
+				                                    InputItem->TextData.Name, StackableAmountAdded),
+			                                    StackableAmountAdded);
+		}
+		if (StackableAmountAdded <= 0)
+		{
+			// return none result
+			return FItemAddResult::AddedNone(FText::Format(
+				FText::FromString("Couldn't add {0} to the inventory. No remaining inventory slots, or invalid item."),
+				InputItem->TextData.Name));
+		}
+	}
+
+	check(false);
+	return FItemAddResult::AddedNone(FText::Format(
+		FText::FromString("Couldn't add {0} to the inventory. No remaining inventory slots, or invalid item."),
+		InputItem->TextData.Name));
+}
+
+void UInventoryComponent::AddNewItem(UItemBase* OldItem, const int32 AmountToAdd)
+{
+	UItemBase* NewItem;
+
+	// 만약 이미 아미템이 복사본이거나 집을 수 있다면
+	if (OldItem->bIsCopy || OldItem->bIsPickup)
+	{
+		NewItem = OldItem;
+		NewItem->ResetItemFlags();
+	}
+	// 그게 아니면
+	else
+	{
+		NewItem = OldItem->CreateItemCopy();
+	}
+	NewItem->OwningInventory = this;
+	NewItem->SetQuantity(AmountToAdd);
+
+	InventoryContents.Add(NewItem);
+	InventoryTotalWeight += NewItem->GetItemStackWeight();
+	OnInventoryUpdated.Broadcast();
+}
---
+++ Source/TheCrusader/Public/Component/Inventory/InventoryComponent.h
@@ -0,0 +1,147 @@
+﻿// Written by jaegang lim
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "InventoryComponent.generated.h"
+
+class UItemBase;
+/** 인벤토리에 아이템을 넣었을 때 아이템 전송 결과 */
+UENUM(BlueprintType)
+enum class EItemAddResult : uint8
+{
+	NoItemAdded,
+	PartialAmountItemAdded,
+	AllTemAdded
+};
+
+USTRUCT(BlueprintType)
+struct FItemAddResult
+{
+	GENERATED_BODY()
+
+	FItemAddResult()
+		: ActualAmountAdded(0),
+		  OperationResult(EItemAddResult::NoItemAdded),
+		  ResultMessage(FText::GetEmpty())
+	{
+	}
+
+	// 인벤토리에 실제로 들어갈 수 있는 갯수
+	UPROPERTY(BlueprintReadOnly, Category = "Item Add Result")
+	int32 ActualAmountAdded;
+	// 아이템 전송 결과
+	UPROPERTY(BlueprintReadOnly, Category = "Item Add Result")
+	EItemAddResult OperationResult;
+	// 결과 메시지
+	UPROPERTY(BlueprintReadOnly, Category = "Item Add Result")
+	FText ResultMessage;
+
+	static FItemAddResult AddedNone(const FText& ErrorText)
+	{
+		FItemAddResult AddedNoneResult;
+		AddedNoneResult.ActualAmountAdded = 0;
+		AddedNoneResult.OperationResult = EItemAddResult::NoItemAdded;
+		AddedNoneResult.ResultMessage = ErrorText;
+		return AddedNoneResult;
+	}
+
+	static FItemAddResult AddedPartial(const FText& ErrorText, const int32 PartialAmountAdded)
+	{
+		FItemAddResult AddedPartialResult;
+		AddedPartialResult.ActualAmountAdded = PartialAmountAdded;
+		AddedPartialResult.OperationResult = EItemAddResult::PartialAmountItemAdded;
+		AddedPartialResult.ResultMessage = ErrorText;
+		return AddedPartialResult;
+	}
+
+	static FItemAddResult AddedAll(const FText& Message, const int32 AmountAdded)
+	{
+		FItemAddResult AddedAllResult;
+		AddedAllResult.ActualAmountAdded = AmountAdded;
+		AddedAllResult.OperationResult = EItemAddResult::AllTemAdded;
+		AddedAllResult.ResultMessage = Message;
+		return AddedAllResult;
+	}
+};
+
+DECLARE_MULTICAST_DELEGATE(FOnInventoryUpdated);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class THECRUSADER_API UInventoryComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UInventoryComponent();
+
+	FOnInventoryUpdated OnInventoryUpdated;
+
+public:
+	UFUNCTION(Category = "Inventory")
+	FItemAddResult HandleAddItem(UItemBase* InputItem);
+
+	UFUNCTION(Category = "Inventory")
+	UItemBase* FindMatchingItem(UItemBase* ItemIn) const;
+	UFUNCTION(Category = "Inventory")
+	UItemBase* FindNextItemByID(UItemBase* ItemIn) const;
+	UFUNCTION(Category = "Inventory")
+	UItemBase* FindNextPartialStack(UItemBase* ItemIn) const;
+
+	UFUNCTION(Category = "Inventory")
+	void RemoveSingleInstanceOfItem(UItemBase* ItemToRemove);
+	UFUNCTION(Category = "Inventory")
+	int32 RemoveAmountOfItem(UItemBase* ItemToRemove, int32 DesiredAmountToRemove);
+	UFUNCTION(Category = "Inventory")
+	void SplitExistingStack(UItemBase* ItemIn, const int32 AmountToSplit);
+
+#pragma region GETTER
+
+	UFUNCTION(Category = "Inventory")
+	FORCEINLINE float GetInventoryTotalWeight() const { return InventoryTotalWeight; }
+
+	UFUNCTION(Category = "Inventory")
+	FORCEINLINE float GetWeightCapacity() const { return InventoryWeightCapacity; }
+
+	UFUNCTION(Category = "Inventory")
+	FORCEINLINE int32 GetSlotCapacity() const { return InventorySlotCapacity; }
+
+	// UFUNCTION 지정자는 TObjectPtr를 처리할 수 없음
+	UFUNCTION(Category = "Inventory")
+	FORCEINLINE TArray<UItemBase*> GetInventoryContents() const { return InventoryContents; }
+
+#pragma endregion GETTER
+
+#pragma region SETTER
+
+	UFUNCTION(Category = "Inventory")
+	FORCEINLINE void SetSlotsCapacity(const int32 NewSlotsCapacity) { InventorySlotCapacity = NewSlotsCapacity; }
+
+	UFUNCTION(Category = "Inventory")
+	FORCEINLINE void SetWeightCapacity(const float NewWeightCapacity) { InventoryWeightCapacity = NewWeightCapacity; }
+
+#pragma endregion SETTER
+
+protected:
+	virtual void BeginPlay() override;
+
+protected:
+	FItemAddResult HandleNonStackableItems(UItemBase*, int32 RequestedAddAmount);
+	int32 HandleStackableItems(UItemBase*, int32 RequestedAddAmount);
+	int32 CalculateWeightAddAmount(UItemBase*, int32 RequestedAddAmount);
+	int32 CalculateNumberForFullStack(UItemBase* StackableItem, int32 RequestedAddAmount);
+
+	void AddNewItem(UItemBase* OldItem, const int32 AmountToAdd);
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category = "Inventory")
+	float InventoryTotalWeight;
+	UPROPERTY(EditInstanceOnly, Category = "Inventory")
+	int32 InventorySlotCapacity;
+	UPROPERTY(VisibleAnywhere, Category = "Inventory")
+	float InventoryWeightCapacity;
+
+	UPROPERTY(VisibleAnywhere, Category = "Inventory")
+	TArray<TObjectPtr<UItemBase>> InventoryContents;
+};
