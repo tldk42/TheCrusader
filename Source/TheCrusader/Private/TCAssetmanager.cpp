463b75e4af9d4954e0ecda5c60b95475a6352ad6
tldk42 <tldk47@hanmail.net>
2023-09-16 10:40:49 +0900

    + AssetManager

---
+++ Source/TheCrusader/Private/TCAssetmanager.cpp
@@ -0,0 +1,26 @@
+﻿// Written by jaegang lim
+
+
+#include "TCAssetmanager.h"
+
+#include "FTCGameplayTags.h"
+
+UTCAssetmanager::UTCAssetmanager()
+{
+}
+
+UTCAssetmanager& UTCAssetmanager::Get()
+{
+	check(GEngine);
+
+	UTCAssetmanager* Assetmanager = Cast<UTCAssetmanager>(GEngine->AssetManager);
+
+	return *Assetmanager;
+}
+
+void UTCAssetmanager::StartInitialLoading()
+{
+	Super::StartInitialLoading();
+
+	FTCGameplayTags::InitializeNativeTags();
+}
---
+++ Source/TheCrusader/Public/TCAssetmanager.h
@@ -0,0 +1,26 @@
+﻿// Written by jaegang lim
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "TCAssetmanager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THECRUSADER_API UTCAssetmanager : public UAssetManager
+{
+	GENERATED_BODY()
+
+public:
+
+	UTCAssetmanager();
+
+	static UTCAssetmanager& Get();
+
+protected:
+
+	virtual void StartInitialLoading() override;
+};
