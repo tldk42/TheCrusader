d934fad4bcb3e5d3cba5e65bd59905a069ed0838
tldk42 <tldk47@hanmail.net>
2023-09-16 10:40:37 +0900

    + GameplayTagContainer

---
+++ Source/TheCrusader/Private/FTCGameplayTags.cpp
@@ -0,0 +1,32 @@
+﻿// Written by jaegang lim
+
+
+#include "FTCGameplayTags.h"
+#include "GameplayTagsManager.h"
+#include "Engine/EngineTypes.h"
+
+FTCGameplayTags FTCGameplayTags::GameplayTags;
+
+void FTCGameplayTags::InitializeNativeTags()
+{
+	UGameplayTagsManager& GameplayTagsManager = UGameplayTagsManager::Get();
+
+	GameplayTags.AddAllTags(GameplayTagsManager);
+
+	GameplayTagsManager.DoneAddingNativeTags();
+}
+
+void FTCGameplayTags::AddAllTags(UGameplayTagsManager& Manager)
+{
+	AddTag(InputTag_LMB, "InputTag.LMB", "Left Mouse Button");
+	AddTag(InputTag_RMB, "InputTag.RMB", "Right Mouse Button");
+	AddTag(InputTag_Spacebar, "InputTag.Spacebar", "Spacebar Button");
+	AddTag(InputTag_E, "InputTag.E", "E Button");
+	AddTag(InputTag_LAlt, "InputTag.LAlt", "LAlt Button");
+}
+
+void FTCGameplayTags::AddTag(FGameplayTag& OutTag, const ANSICHAR* TagName, const ANSICHAR* TagComment)
+{
+	OutTag = UGameplayTagsManager::Get().AddNativeGameplayTag(FName(TagName),
+	                                                          FString(TEXT("(Native) ")) + FString(TagComment));
+}
---
+++ Source/TheCrusader/Public/FTCGameplayTags.h
@@ -0,0 +1,37 @@
+﻿// Written by jaegang lim
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+class UGameplayTagsManager;
+
+/**
+* FTCGameplayTags
+*
+*	Singleton containing native gameplay tags.
+*/
+class THECRUSADER_API FTCGameplayTags
+{
+public:
+
+	static const FTCGameplayTags& Get() {return GameplayTags;}
+
+	static void InitializeNativeTags();
+
+	FGameplayTag InputTag_LMB;
+	FGameplayTag InputTag_RMB;
+	FGameplayTag InputTag_Spacebar;
+	FGameplayTag InputTag_E;
+	FGameplayTag InputTag_LAlt;
+
+protected:
+	void AddAllTags(UGameplayTagsManager& Manager);
+
+	void AddTag(FGameplayTag& OutTag, const ANSICHAR* TagName, const ANSICHAR* TagComment);
+
+private:
+
+	static FTCGameplayTags GameplayTags;
+};
