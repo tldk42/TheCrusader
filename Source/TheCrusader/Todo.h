76da272bce2d91c27b48a335ccfbef55e25379fd
JacobLim <83587015+tldk42@users.noreply.github.com>
2023-09-21 13:59:14 +0900

    Create Todo.h
    9.21 TODO : InventoryActionMenu

---
+++ Source/TheCrusader/Todo.h
@@ -0,0 +1,5 @@
+// TODO: InventoryActionMenu 위젯에서 렌더링 기능만 수행하도록 변경
+// ActionMenu 위젯에서 Actor를 직접 생성해서 드롭하는 메서드가 추가되어있는데 이 개체의 목적과같지 않음
+
+// TODO: InventoryActionMenu의 Spawn() 삭제
+// TODO: 버튼 클릭시 발동되는 기능은 직접 ItemBase의 Use에서 처리하도록 변경
