4c70a46e93621f3d90a6b19bb55664f32d7db17f
tldk42 <tldk47@hanmail.net>
2023-09-21 10:26:20 +0900

    + InventoryActionMenu (슬롯 클릭시 액션 창 생성)

---
+++ Source/TheCrusader/Private/UI/Inventory/InventoryActionMenu.cpp
@@ -0,0 +1,108 @@
+﻿// Written by jaegang lim
+
+
+#include "UI/Inventory/InventoryActionMenu.h"
+
+#include "Blueprint/WidgetLayoutLibrary.h"
+#include "Character/Balian.h"
+#include "Component/Inventory/InventoryComponent.h"
+#include "Components/Button.h"
+#include "Components/CanvasPanelSlot.h"
+#include "Components/VerticalBox.h"
+#include "Item/ItemPickup.h"
+#include "Data/ItemDataStructs.h"
+#include "Item/Data/ItemBase.h"
+#include "Item/Weapon/MyItem.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "UI/Inventory/InventoryItemSlot.h"
+
+void UInventoryActionMenu::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	UWidgetLayoutLibrary::SlotAsCanvasSlot(BoxActionMenu)->SetPosition(
+		UWidgetLayoutLibrary::GetMousePositionOnViewport(this));
+
+	BtnUse->OnClicked.AddDynamic(this, &ThisClass::Use);
+	BtnDrop->OnClicked.AddDynamic(this, &ThisClass::Drop);
+	BtnDropAll->OnClicked.AddDynamic(this, &ThisClass::DropAll);
+}
+
+void UInventoryActionMenu::NativeOnMouseLeave(const FPointerEvent& InMouseEvent)
+{
+	Super::NativeOnMouseLeave(InMouseEvent);
+	RemoveFromParent();
+}
+
+void UInventoryActionMenu::SpawnItem(int32 Quantity) const
+{
+	UItemBase* Item = SourceSlot->ItemReference;
+	if (Item)
+	{
+		if (Item->OwningInventory && Item->OwningInventory->FindMatchingItem(Item))
+		{
+			APawn* Pawn = GetOwningPlayerPawn();
+
+			FHitResult HitResult;
+			FVector Start =
+				Pawn->GetActorLocation() +
+				UKismetMathLibrary::RandomUnitVectorInConeInDegrees(
+					Pawn->GetActorForwardVector(), 30.f) * 150.f;
+			FVector End = Start - FVector(0, 0, 500.f);
+
+			if (GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, ECC_Visibility))
+			{
+				const int32 RemovedQuantity = Item->OwningInventory->RemoveAmountOfItem(Item, Quantity);
+				if (Item->ItemType == EItemType::Weapon)
+				{
+					AMyItem* WeaponItem = GetWorld()->SpawnActor<AMyItem>(
+						AMyItem::StaticClass(), HitResult.Location, FRotator::ZeroRotator);
+					WeaponItem->InitializeDrop(Item, RemovedQuantity);
+				}
+				else
+				{
+					AItemPickup* PickupItem = GetWorld()->SpawnActor<AItemPickup>(
+						AItemPickup::StaticClass(), HitResult.Location, FRotator::ZeroRotator);
+					PickupItem->InitializeDrop(Item, RemovedQuantity);
+				}
+			}
+		}
+		else
+		{
+			UE_LOG(LogTemp, Error, TEXT("Can not remove item from inventory"));
+		}
+	}
+}
+
+void UInventoryActionMenu::Use()
+{
+	UItemBase* Item = SourceSlot->ItemReference;
+	if (Item)
+	{
+		ABalian* Player = Cast<ABalian>(GetOwningPlayerPawn());
+		if (Player)
+		{
+			Item->Use(Player);
+		}
+
+		Player->GetInventory()->RemoveAmountOfItem(Item, 1);
+	}
+
+	RemoveFromParent();
+}
+
+void UInventoryActionMenu::Drop()
+{
+	SpawnItem(1);
+	RemoveFromParent();
+}
+
+void UInventoryActionMenu::DropAll()
+{
+	UItemBase* Item = SourceSlot->ItemReference;
+	if (Item)
+	{
+		SpawnItem(Item->Quantity);
+	}
+	RemoveFromParent();
+}
---
+++ Source/TheCrusader/Public/UI/Inventory/InventoryActionMenu.h
@@ -0,0 +1,48 @@
+﻿// Written by jaegang lim
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InventoryActionMenu.generated.h"
+
+class UInventoryItemSlot;
+class UVerticalBox;
+class UButton;
+class UHorizontalBox;
+/**
+ * 
+ */
+UCLASS()
+class THECRUSADER_API UInventoryActionMenu : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void NativeConstruct() override;
+	virtual void NativeOnMouseLeave(const FPointerEvent& InMouseEvent) override;
+
+private:
+	void SpawnItem(int32 Quantity) const;
+	
+	UFUNCTION()
+	void Use();
+	UFUNCTION()
+	void Drop();
+	UFUNCTION()
+	void DropAll();
+
+public:
+	UPROPERTY(meta = (BindWidget))
+	UVerticalBox* BoxActionMenu;
+	UPROPERTY(meta = (BindWidget))
+	UButton* BtnUse;
+	UPROPERTY(meta = (BindWidget))
+	UButton* BtnDrop;
+	UPROPERTY(meta = (BindWidget))
+	UButton* BtnDropAll;
+
+
+	UPROPERTY()
+	UInventoryItemSlot* SourceSlot;
+};
