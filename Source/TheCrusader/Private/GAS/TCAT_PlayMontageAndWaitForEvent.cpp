673613b7adb7d52c48e3ab2e06b8c726aec69699
tldk42 <tldk47@hanmail.net>
2023-09-23 00:39:03 +0900

    + PlayMontageAndWait
    - GameplayTask
    - 몽타주 진행 후 GameplayEvent대기

---
+++ Source/TheCrusader/Private/GAS/TCAT_PlayMontageAndWaitForEvent.cpp
@@ -0,0 +1,271 @@
+﻿// Written by jaegang lim
+
+
+#include "GAS/TCAT_PlayMontageAndWaitForEvent.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+#include "GameFramework/Character.h"
+#include "GAS/TCAbilitySystemComponent.h"
+
+UTCAT_PlayMontageAndWaitForEvent::UTCAT_PlayMontageAndWaitForEvent(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	Rate = 1.f;
+	bStopWhenAbilityEnds = true;
+}
+
+UTCAbilitySystemComponent* UTCAT_PlayMontageAndWaitForEvent::GetTargetASC()
+{
+	return Cast<UTCAbilitySystemComponent>(AbilitySystemComponent);
+}
+
+void UTCAT_PlayMontageAndWaitForEvent::OnMontageBlendingOut(UAnimMontage* Montage, bool bInterrupted)
+{
+	if (Ability && Ability->GetCurrentMontage() == MontageToPlay)
+	{
+		if (Montage == MontageToPlay)
+		{
+			AbilitySystemComponent->ClearAnimatingAbility(Ability);
+
+			ACharacter* Character = Cast<ACharacter>(GetAvatarActor());
+			if (Character && (Character->GetLocalRole() == ROLE_Authority)
+				|| (Character->GetLocalRole() == ROLE_AutonomousProxy && Ability->GetNetExecutionPolicy() ==
+					EGameplayAbilityNetExecutionPolicy::LocalPredicted))
+			{
+				Character->SetAnimRootMotionTranslationScale(1.f);
+			}
+		}
+	}
+
+	if (bInterrupted)
+	{
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			OnInterrupted.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+	else
+	{
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			OnBlendOut.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+}
+
+void UTCAT_PlayMontageAndWaitForEvent::OnAbilityCancelled()
+{
+
+	if (StopPlayingMontage())
+	{
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			OnCancelled.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+}
+
+void UTCAT_PlayMontageAndWaitForEvent::OnMontageEnded(UAnimMontage* Montage, bool bInterrupted)
+{
+	if (!bInterrupted)
+	{
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			OnCompleted.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+}
+
+void UTCAT_PlayMontageAndWaitForEvent::OnGameplayEvent(FGameplayTag EventTag, const FGameplayEventData* Payload)
+{
+	// 현재 액티브 상태인지 확인
+	// 만약 Task가 비활성화 상태(취소, 완료)라면 이 이벤트를 처리하지 않는다.
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		FGameplayEventData TempData = *Payload;
+		TempData.EventTag = EventTag;
+
+		EventReceived.Broadcast(EventTag, TempData);
+	}
+}
+
+void UTCAT_PlayMontageAndWaitForEvent::Activate()
+{
+	if (Ability == nullptr)
+		return;
+
+	bool bPlayedMontage = false;
+
+	if (AbilitySystemComponent.IsValid())
+	{
+		const FGameplayAbilityActorInfo* ActorInfo = Ability->GetCurrentActorInfo();
+		UAnimInstance* AnimInstance = ActorInfo->GetAnimInstance();
+
+		// 몽타주를 실행하고 Event, Cancelled, BlendOut, Ended에 델리게이트를 바인딩한다.
+		if (AnimInstance != nullptr)
+		{
+			// 아래 EventTag에 해당하는 이벤트가 발생할때마다 OnGameplayEvent를 발생
+			EventHandle = AbilitySystemComponent->AddGameplayEventTagContainerDelegate(
+				EventTags,
+				FGameplayEventTagMulticastDelegate::FDelegate::CreateUObject(this, &ThisClass::OnGameplayEvent));
+
+			if (AbilitySystemComponent->PlayMontage(Ability, Ability->GetCurrentActivationInfo(), MontageToPlay, Rate,
+			                                        StartSection) > 0.f)
+			{
+				if (ShouldBroadcastAbilityTaskDelegates() == false)
+				{
+					return;
+				}
+
+				CancelledHandle = Ability->OnGameplayAbilityCancelled.AddUObject(this, &ThisClass::OnAbilityCancelled);
+
+				BlendingOutDelegate.BindUObject(this, &ThisClass::OnMontageBlendingOut);
+				AnimInstance->Montage_SetBlendingOutDelegate(BlendingOutDelegate, MontageToPlay);
+
+				MontageEndedDelegate.BindUObject(this, &ThisClass::OnMontageEnded);
+				AnimInstance->Montage_SetEndDelegate(MontageEndedDelegate, MontageToPlay);
+
+				ACharacter* Character = Cast<ACharacter>(GetAvatarActor());
+				if (Character && (Character->GetLocalRole() == ROLE_Authority ||
+					(Character->GetLocalRole() == ROLE_AutonomousProxy && Ability->GetNetExecutionPolicy() ==
+						EGameplayAbilityNetExecutionPolicy::LocalPredicted)))
+				{
+					Character->SetAnimRootMotionTranslationScale(AnimRootMotionTranslationScale);
+				}
+
+				bPlayedMontage = true;
+			}
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("AbilityTask_PlayMontageAndWaitForEvent call to PlayMontage failed!"));
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning,
+		       TEXT("AbilityTask_PlayMontageAndWaitForEvent called on invalid AbilitySystemComponent"));
+	}
+
+	if (!bPlayedMontage)
+	{
+		UE_LOG(LogTemp, Warning,
+		       TEXT(
+			       "AbilityTask_PlayMontageAndWaitForEvent called in Ability %s failed to play montage %s; Task Instance Name %s."
+		       ), *Ability->GetName(), *GetNameSafe(MontageToPlay), *InstanceName.ToString());
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			//ABILITY_LOG(Display, TEXT("%s: OnCancelled"), *GetName());
+			OnCancelled.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+	SetWaitingOnAvatar();
+}
+
+void UTCAT_PlayMontageAndWaitForEvent::ExternalCancel()
+{
+	check(AbilitySystemComponent.IsValid());
+
+	OnAbilityCancelled();
+
+	Super::ExternalCancel();
+}
+
+void UTCAT_PlayMontageAndWaitForEvent::OnDestroy(bool bInOwnerFinished)
+{
+	// 이전 몽타주에 바인딩된 Delegate들은 알아서 해제된다.
+	// 하지만 Multicast Delegate들은 한 번에 여러 개의 대상을 호출하므로
+	// 직접 해줘야한다.
+	// 대상 : CancelledHandle | EventHandle
+	if (Ability)
+	{
+		Ability->OnGameplayAbilityCancelled.Remove(CancelledHandle);
+		if (bInOwnerFinished && bStopWhenAbilityEnds)
+		{
+			StopPlayingMontage();
+		}
+	}
+
+	if (AbilitySystemComponent.IsValid())
+	{
+		AbilitySystemComponent->RemoveGameplayEventTagContainerDelegate(EventTags, EventHandle);
+	}
+
+	Super::OnDestroy(bInOwnerFinished);
+}
+
+bool UTCAT_PlayMontageAndWaitForEvent::StopPlayingMontage()
+{
+	const FGameplayAbilityActorInfo* ActorInfo = Ability->GetCurrentActorInfo();
+	if (!ActorInfo)
+	{
+		return false;
+	}
+
+	UAnimInstance* AnimInstance = ActorInfo->GetAnimInstance();
+	if (AnimInstance == nullptr)
+	{
+		return false;
+	}
+
+	if (AbilitySystemComponent.IsValid() && Ability)
+	{
+		if (AbilitySystemComponent->GetAnimatingAbility() == Ability && AbilitySystemComponent->GetCurrentMontage() ==
+			MontageToPlay)
+		{
+			FAnimMontageInstance* MontageInstance = AnimInstance->GetActiveInstanceForMontage(MontageToPlay);
+			if (MontageInstance)
+			{
+				MontageInstance->OnMontageBlendingOutStarted.Unbind();
+				MontageInstance->OnMontageEnded.Unbind();
+			}
+
+			AbilitySystemComponent->CurrentMontageStop();
+			return true;
+		}
+	}
+
+	return false;
+}
+
+UTCAT_PlayMontageAndWaitForEvent* UTCAT_PlayMontageAndWaitForEvent::PlayMontageAndWaitForEvent(
+	UGameplayAbility* OwningAbility, FName TaskInstanceName, UAnimMontage* MontageToPlay,
+	FGameplayTagContainer EventTags, float Rate, FName StartSection, bool bStopWhenAbilityEnds,
+	float AnimRootMotionTranslationScale)
+{
+	UAbilitySystemGlobals::NonShipping_ApplyGlobalAbilityScaler_Rate(Rate);
+
+	UTCAT_PlayMontageAndWaitForEvent* MyObj = NewAbilityTask<UTCAT_PlayMontageAndWaitForEvent>(
+		OwningAbility, TaskInstanceName);
+	MyObj->MontageToPlay = MontageToPlay;
+	MyObj->EventTags = EventTags;
+	MyObj->Rate = Rate;
+	MyObj->StartSection = StartSection;
+	MyObj->AnimRootMotionTranslationScale = AnimRootMotionTranslationScale;
+	MyObj->bStopWhenAbilityEnds = bStopWhenAbilityEnds;
+
+	return MyObj;
+}
+
+FString UTCAT_PlayMontageAndWaitForEvent::GetDebugString() const
+{
+	UAnimMontage* PlayingMontage = nullptr;
+	if (Ability)
+	{
+		const FGameplayAbilityActorInfo* ActorInfo = Ability->GetCurrentActorInfo();
+		UAnimInstance* AnimInstance = ActorInfo->GetAnimInstance();
+
+		if (AnimInstance != nullptr)
+		{
+			PlayingMontage = AnimInstance->Montage_IsActive(MontageToPlay)
+								 ? MontageToPlay
+								 : AnimInstance->GetCurrentActiveMontage();
+		}
+	}
+
+	return FString::Printf(
+		TEXT("PlayMontageAndWaitForEvent. MontageToPlay: %s  (Currently Playing): %s"), *GetNameSafe(MontageToPlay),
+		*GetNameSafe(PlayingMontage));
+}
+
---
+++ Source/TheCrusader/Public/GAS/TCAT_PlayMontageAndWaitForEvent.h
@@ -0,0 +1,131 @@
+﻿// Written by jaegang lim
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "TCAT_PlayMontageAndWaitForEvent.generated.h"
+
+class UTCAbilitySystemComponent;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FTCPlayMontageAndWaitForEventDelegate, FGameplayTag, EventTag,
+                                             FGameplayEventData, EventData);
+
+
+/**
+ *  PlayMontageAndWait & WaitForEvent를 조합한 Task
+ */
+UCLASS()
+class THECRUSADER_API UTCAT_PlayMontageAndWaitForEvent : public UAbilityTask
+{
+	GENERATED_BODY()
+
+public:
+#pragma region Constructor&Overrides
+	UTCAT_PlayMontageAndWaitForEvent(const FObjectInitializer& ObjectInitializer);
+	virtual void Activate() override;
+	virtual void ExternalCancel() override;
+	virtual FString GetDebugString() const override;
+	virtual void OnDestroy(bool bInOwnerFinished) override;
+#pragma endregion Constructor&Overrides
+
+#pragma region Delegates
+	
+	/** Complete */
+	UPROPERTY(BlueprintAssignable)
+	FTCPlayMontageAndWaitForEventDelegate OnCompleted;
+
+	/** Blending Out */
+	UPROPERTY(BlueprintAssignable)
+	FTCPlayMontageAndWaitForEventDelegate OnBlendOut;
+
+	/** Interrupted */
+	UPROPERTY(BlueprintAssignable)
+	FTCPlayMontageAndWaitForEventDelegate OnInterrupted;
+
+	/** Cancelled */
+	UPROPERTY(BlueprintAssignable)
+	FTCPlayMontageAndWaitForEventDelegate OnCancelled;
+
+	/** Event Received */
+	UPROPERTY(BlueprintAssignable)
+	FTCPlayMontageAndWaitForEventDelegate EventReceived;
+
+#pragma endregion Delegates
+
+	/**
+	 * 몽타주를 재생하고 끝나기까지 기다린다.
+	 * 특정이벤트 태그와 일치하는 이벤트(또는 비어있는)가 발생하게 되면,
+	 * EventReceived 델리게이트가 관련된 함수들을 실행시킨다.
+	 * If StopWhenAbilityEnds is true, this montage will be aborted if the ability ends normally.
+	 * It is always stopped when the ability is explicitly cancelled.
+	 * On normal execution, OnBlendOut is called when the montage is blending out, and OnCompleted when it is completely done playing
+	 * OnInterrupted is called if another montage overwrites this, and OnCancelled is called if the ability or task is cancelled
+	 *
+	 * @param OwningAbility
+	 * @param TaskInstanceName Set to override the name of this task, for later querying
+	 * @param MontageToPlay The montage to play on the character
+	 * @param EventTags Any gameplay events matching this tag will activate the EventReceived callback. If empty, all events will trigger callback
+	 * @param Rate Change to play the montage faster or slower
+	 * @param StartSection
+	 * @param bStopWhenAbilityEnds If true, this montage will be aborted if the ability ends normally. It is always stopped when the ability is explicitly cancelled
+	 * @param AnimRootMotionTranslationScale Change to modify size of root motion or set to 0 to block it entirely
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Ability|Tasks",
+		meta = (HidePin = "OwningAbility", DefaultToSelf = "OwningAbility", BlueprintInternalUseOnly = "TRUE"))
+	static UTCAT_PlayMontageAndWaitForEvent* PlayMontageAndWaitForEvent(
+		UGameplayAbility* OwningAbility,
+		FName TaskInstanceName,
+		UAnimMontage* MontageToPlay,
+		FGameplayTagContainer EventTags,
+		float Rate = 1.f,
+		FName StartSection = NAME_None,
+		bool bStopWhenAbilityEnds = true,
+		float AnimRootMotionTranslationScale = 1.f);
+
+private:
+
+#pragma region MontageProperties
+	
+	/** 재생할 몽타주 */
+	UPROPERTY()
+	UAnimMontage* MontageToPlay;
+
+	/** 이벤트 태그 컨테이너 */
+	UPROPERTY()
+	FGameplayTagContainer EventTags;
+
+	/** 몽타주 속도 */
+	UPROPERTY()
+	float Rate;
+
+	/** 몽타주 시작 섹션 */
+	UPROPERTY()
+	FName StartSection;
+
+	/** 루트모션 강도 */
+	UPROPERTY()
+	float AnimRootMotionTranslationScale;
+
+	/** 어빌리티 끝나면 몽타주 끄기 */
+	UPROPERTY()
+	bool bStopWhenAbilityEnds;
+
+#pragma endregion MontageProperties
+
+	
+	/** 몽타주를 정지시킨다. */
+	bool StopPlayingMontage();
+
+	/** ASC 반환 */
+	UTCAbilitySystemComponent* GetTargetASC();
+
+	void OnMontageBlendingOut(UAnimMontage* Montage, bool bInterrupted);
+	void OnAbilityCancelled();
+	void OnMontageEnded(UAnimMontage* Montage, bool bInterrupted);
+	void OnGameplayEvent(FGameplayTag EventTag, const FGameplayEventData* Payload);
+
+	FOnMontageBlendingOutStarted BlendingOutDelegate;
+	FOnMontageEnded MontageEndedDelegate;
+	FDelegateHandle CancelledHandle;
+	FDelegateHandle EventHandle;
+};
