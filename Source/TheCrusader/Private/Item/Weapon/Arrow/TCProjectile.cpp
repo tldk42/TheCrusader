a1318681e6ce2dc9bc490192f8fa875d6fa272c6
tldk42 <tldk47@hanmail.net>
2023-10-23 08:23:23 +0900

    + Arrow Projectile

---
+++ Source/TheCrusader/Private/Item/Weapon/Arrow/TCProjectile.cpp
@@ -0,0 +1,21 @@
+﻿// Written by jaegang lim
+
+
+#include "Item/Weapon/Arrow/TCProjectile.h"
+
+#include "GameFramework/ProjectileMovementComponent.h"
+
+
+ATCProjectile::ATCProjectile()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	bReplicates = true;
+
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(FName("ProjectileMovement"));
+}
+
+void ATCProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+}
---
+++ Source/TheCrusader/Public/Item/Weapon/Arrow/TCProjectile.h
@@ -0,0 +1,30 @@
+﻿// Written by jaegang lim
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectTypes.h"
+#include "GameFramework/Actor.h"
+#include "TCProjectile.generated.h"
+
+UCLASS()
+class THECRUSADER_API ATCProjectile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ATCProjectile();
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Meta = (ExposeOnSpawn = true))
+	float Range;
+
+	UPROPERTY(BlueprintReadWrite, Meta = (ExposeOnSpawn = true))
+	FGameplayEffectSpecHandle DamageEffectSpecHandle;
+	
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	class UProjectileMovementComponent* ProjectileMovement;
+
+protected:
+	virtual void BeginPlay() override;
+
+};
