f4bc77b7e6f2469ac9af18ddba1c8f37fdba7782
tldk42 <tldk47@hanmail.net>
2023-10-26 20:35:28 +0900

    * Base 캐릭터 클래스수정 - Base클래스에서 ModularPart 변경

--- Source/TheCrusader/Private/Character/TCCharacterBase.cpp
+++ Source/TheCrusader/Private/Character/TCCharacterBase.cpp
@@ -8,14 +8,57 @@
 
 ATCCharacterBase::ATCCharacterBase()
 {
+	Body = CreateDefaultSubobject<USkeletalMeshComponent>("Body");
+	Face = CreateDefaultSubobject<USkeletalMeshComponent>("Face");
+	Beard = CreateDefaultSubobject<USkeletalMeshComponent>("Extra");
+	Torso = CreateDefaultSubobject<USkeletalMeshComponent>("Torso");
+	Arm = CreateDefaultSubobject<USkeletalMeshComponent>("Arm");
+	Leg = CreateDefaultSubobject<USkeletalMeshComponent>("Leg");
+	Feet = CreateDefaultSubobject<USkeletalMeshComponent>("Feet");
+	Hair = CreateDefaultSubobject<USkeletalMeshComponent>("Hair");
+	Hat = CreateDefaultSubobject<USkeletalMeshComponent>("Hat");
+
+	ShieldMesh = CreateDefaultSubobject<UStaticMeshComponent>("Shield");
+	SwordZip = CreateDefaultSubobject<UStaticMeshComponent>("SwordZip");
+	LongswordZip = CreateDefaultSubobject<UStaticMeshComponent>("LongswordZip");
+	Quiver = CreateDefaultSubobject<UStaticMeshComponent>("Quiver");
+
+	Body->SetupAttachment(GetMesh());
+	Face->SetupAttachment(GetMesh());
+	Beard->SetupAttachment(GetMesh());
+	Torso->SetupAttachment(GetMesh());
+	Arm->SetupAttachment(GetMesh());
+	Leg->SetupAttachment(GetMesh());
+	Feet->SetupAttachment(GetMesh());
+	Hair->SetupAttachment(GetMesh());
+	Hat->SetupAttachment(GetMesh());
+
+	ShieldMesh->SetupAttachment(GetMesh(), TEXT("shield_equip"));
+	SwordZip->SetupAttachment(GetMesh(), TEXT("sword_equip"));
+	LongswordZip->SetupAttachment(GetMesh(), TEXT("longsword_equip"));
+	Quiver->SetupAttachment(GetMesh(), TEXT("QuiverSocket"));
+
+	ShieldMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	SwordZip->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	LongswordZip->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	Quiver->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	ShieldMesh->SetVisibility(false);
+	SwordZip->SetVisibility(false);
+	LongswordZip->SetVisibility(false);
+	Quiver->SetVisibility(false);
+
+	UpdateMorphTargets();
+	SetMasterPoseComponentForParts();
+
 	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
 
 	bUseControllerRotationPitch = false;
 	bUseControllerRotationYaw = false;
 	bUseControllerRotationRoll = false;
 
-	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
-	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f); // ...at this rotation rate
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f);
 
 	GetCharacterMovement()->JumpZVelocity = 700.f;
 	GetCharacterMovement()->AirControl = 0.35f;
@@ -24,6 +67,52 @@ ATCCharacterBase::ATCCharacterBase()
 	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
 }
 
+void ATCCharacterBase::SetMasterPoseComponentForParts() const
+{
+	Body->SetLeaderPoseComponent(GetMesh());
+	Face->SetLeaderPoseComponent(GetMesh());
+	Beard->SetLeaderPoseComponent(GetMesh());
+	Torso->SetLeaderPoseComponent(GetMesh());
+	Arm->SetLeaderPoseComponent(GetMesh());
+	Leg->SetLeaderPoseComponent(GetMesh());
+	Feet->SetLeaderPoseComponent(GetMesh());
+	Hair->SetLeaderPoseComponent(GetMesh());
+	Hat->SetLeaderPoseComponent(GetMesh());
+}
 
+float ATCCharacterBase::GetMorphTargetValue(const bool bCanMorph)
+{
+	return bCanMorph ? 1.f : 0.f;
+}
 
+void ATCCharacterBase::UpdateMorphTargets()
+{
+	USkeletalMeshComponent* BodyParts[8];
 
+	BodyParts[0] = Body;
+	BodyParts[1] = Face;
+	BodyParts[2] = Beard;
+	BodyParts[3] = Torso;
+	BodyParts[4] = Arm;
+	BodyParts[5] = Leg;
+	BodyParts[6] = Feet;
+	BodyParts[7] = Hair;
+	BodyParts[8] = Hat;
+
+	for (USkeletalMeshComponent* Part : BodyParts)
+	{
+		if (Part)
+		{
+			if (UAnimInstance* AnimInstance = Part->GetAnimInstance())
+			{
+				AnimInstance->SetMorphTarget("hat_equipped", GetMorphTargetValue(bHatEquipped));
+				AnimInstance->SetMorphTarget("gloves_equipped", GetMorphTargetValue(bGlovesEquipped));
+			}
+			else
+			{
+				Part->SetMorphTarget("hat_equipped", GetMorphTargetValue(bHatEquipped), true);
+				Part->SetMorphTarget("gloves_equipped", GetMorphTargetValue(bGlovesEquipped), true);
+			}
+		}
+	}
+}
--- Source/TheCrusader/Public/Character/TCCharacterBase.h
+++ Source/TheCrusader/Public/Character/TCCharacterBase.h
@@ -13,27 +13,56 @@ class ATCCharacterBase : public ACharacter
 {
 	GENERATED_BODY()
 
-protected:
-	/** Camera boom positioning the camera behind the character */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class USpringArmComponent* CameraBoom;
-
-	/** Follow camera */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* FollowCamera;
-
 public:
 	ATCCharacterBase();
 
+	virtual float GetMorphTargetValue(bool bCanMorph);
 
-public:
-	/** Returns CameraBoom subobject **/
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
-	/** Returns FollowCamera subobject **/
-	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+protected:
+	void SetMasterPoseComponentForParts() const;
+	UFUNCTION(BlueprintCallable)
+	void UpdateMorphTargets();
 
-private:
+protected:
+#pragma region Modular Parts
+
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
-	float BaseTurnRate = 1.f;
-};
+	USkeletalMeshComponent* Body;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	USkeletalMeshComponent* Face;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	USkeletalMeshComponent* Beard;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	USkeletalMeshComponent* Torso;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	USkeletalMeshComponent* Arm;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	USkeletalMeshComponent* Leg;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	USkeletalMeshComponent* Feet;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	USkeletalMeshComponent* Hair;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	USkeletalMeshComponent* Hat;
 
+#pragma endregion Modular Parts
+
+#pragma region Equipment Accessory
+
+	UPROPERTY(EditDefaultsOnly, Category = "Armour")
+	UStaticMeshComponent* ShieldMesh;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Armour")
+	UStaticMeshComponent* LongswordZip;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Armour")
+	UStaticMeshComponent* SwordZip;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Armour", meta = (AllowPrivateAccess = true))
+	UStaticMeshComponent* Quiver;
+#pragma endregion Equipment Accessory
+
+	bool bHatEquipped = false;
+
+	bool bGlovesEquipped = false;
+};
