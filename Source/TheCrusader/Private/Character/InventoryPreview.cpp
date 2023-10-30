8352844ee8549ba846d1cae80d0563dc1689d979
tldk42 <tldk47@hanmail.net>
2023-10-31 02:17:07 +0900

    + Inventory Preview Character

---
+++ Source/TheCrusader/Private/Character/InventoryPreview.cpp
@@ -0,0 +1,83 @@
+﻿// Written by jaegang lim
+
+
+#include "Character/InventoryPreview.h"
+
+#include "Character/Balian.h"
+#include "Components/SceneCaptureComponent2D.h"
+#include "Item/Data/ItemEquipmentBase.h"
+#include "Item/Data/ItemWeaponBase.h"
+
+
+AInventoryPreview::AInventoryPreview(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SceneCaptureComponent2D = CreateDefaultSubobject<USceneCaptureComponent2D>("Capture");
+	PreviewWeapon = CreateDefaultSubobject<UStaticMeshComponent>("PreviewWeapon");
+	PreviewBow = CreateDefaultSubobject<USkeletalMeshComponent>("PreviewBow");
+
+	SceneCaptureComponent2D->SetupAttachment(GetRootComponent());
+	PreviewWeapon->SetupAttachment(GetMesh());
+	PreviewBow->SetupAttachment(GetMesh(), "bow_equip");
+
+	UpdateMorphTargets();
+	SetMasterPoseComponentForParts();
+	SetTickableWhenPaused(true);
+	SceneCaptureComponent2D->SetTickableWhenPaused(true);
+}
+
+void AInventoryPreview::AttachEquipment(const EEquipmentPart EquipmentPart, const UItemEquipmentBase* ItemToEquip)
+{
+	Super::AttachEquipment(EquipmentPart, ItemToEquip);
+
+	if (EquipmentPart == EEquipmentPart::Weapon)
+	{
+		if (const UItemWeaponBase* Weapon = Cast<UItemWeaponBase>(ItemToEquip))
+		{
+			PreviewWeapon->SetStaticMesh(ItemToEquip->AssetData.Mesh);
+			switch (Weapon->WeaponData.Type)
+			{
+			case EWeaponType::TwoHandSword:
+				PreviewWeapon->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetIncludingScale,
+				                                 "longsword_equip");
+				break;
+			case EWeaponType::OneHandSword:
+				PreviewWeapon->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetIncludingScale,
+				                                 "sword_equip");
+				break;
+			case EWeaponType::Spear:
+				PreviewWeapon->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetIncludingScale,
+				                                 "spear");
+				break;
+			case EWeaponType::Hammer:
+				break;
+			default: ;
+			}
+		}
+	}
+	else if (EquipmentPart == EEquipmentPart::Bow)
+	{
+		PreviewBow->SetSkeletalMesh(ItemToEquip->AssetData.AnimatedMesh);
+	}
+}
+
+void AInventoryPreview::DetachEquipment(EEquipmentPart EquipmentPart)
+{
+	Super::DetachEquipment(EquipmentPart);
+
+	if (EquipmentPart == EEquipmentPart::Weapon)
+	{
+		PreviewWeapon->SetStaticMesh(nullptr);
+	}
+	else if (EquipmentPart == EEquipmentPart::Bow)
+	{
+		PreviewBow->SetSkeletalMesh(nullptr);
+	}
+}
+
+void AInventoryPreview::BeginPlay()
+{
+	Super::BeginPlay();
+}
---
+++ Source/TheCrusader/Public/Character/InventoryPreview.h
@@ -0,0 +1,36 @@
+﻿// Written by jaegang lim
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TCCharacterBase.h"
+#include "InventoryPreview.generated.h"
+
+class UItemEquipmentBase;
+class ABalian;
+
+UCLASS()
+class THECRUSADER_API AInventoryPreview : public ATCCharacterBase
+{
+	GENERATED_BODY()
+
+public:
+	AInventoryPreview(const FObjectInitializer& ObjectInitializer);
+
+	virtual void AttachEquipment(const EEquipmentPart EquipmentPart, const UItemEquipmentBase* ItemToEquip) override;
+	virtual void DetachEquipment(EEquipmentPart EquipmentPart) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+
+private:
+	UPROPERTY(EditDefaultsOnly)
+	USceneCaptureComponent2D* SceneCaptureComponent2D;
+
+	UPROPERTY(EditDefaultsOnly, Category = "ACC")
+	UStaticMeshComponent* PreviewWeapon;
+	UPROPERTY(EditDefaultsOnly, Category = "ACC")
+	USkeletalMeshComponent* PreviewBow;
+};
