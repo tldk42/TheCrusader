c540a069b7cdd2bae175817c5b3eb9f65949803c
tldk42 <tldk47@hanmail.net>
2023-10-31 02:14:35 +0900

    + UI: DamageText 타격시 텍스트 위젯 추가

---
+++ Source/TheCrusader/Public/UI/DamageText/DamageText.h
@@ -0,0 +1,27 @@
+﻿// Written by jaegang lim
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "DamageText.generated.h"
+
+class UTextBlock;
+/**
+ * 
+ */
+UCLASS()
+class THECRUSADER_API UDamageText : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadOnly)
+	float DamageAmount;
+
+	UPROPERTY(BlueprintReadOnly)
+	FVector2D WidgetPos;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UTextBlock* DamageText;
+};
